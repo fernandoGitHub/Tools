2d741911d739ce6ffa2431e6b798167835496811
fernandoGitHub <fernando.funtowicz@gmail.com>
2022-08-07 15:16:10 +0300

    ss

--- packaging/src/tools_fernandoGitHub/ff_data_transfer.py
+++ packaging/src/tools_fernandoGitHub/ff_data_transfer.py
@@ -3,6 +3,24 @@ import shutil
 from zipfile import ZipFile
 
 def retrieve_data(data_instructions):
+  '''
+  Retrieves data from different sources including GitHub and GoogleDrive
+  If the file is Zip, then it automatically decompresses it
+  Arguments:
+     - data instructions (list of 5 element sets) - Every element will provide information for a single operation
+        -- base_path (str) - the directory where the specific file will be transferred, not including the filename
+        -- file (str) - name of the file to be transferred
+        -- base_url (str) - the URL path not including the filename
+        -- method (str) - 'Gmail' or 'Github'
+        -- remove (boolen) - If true, removes the base path dir before copying again
+        
+   Example:
+     from tools_fernandogithub import ff_data_transfer as ffdt
+     data_instructions=list()
+     data_instructions.append(('.', 'XML.zip', 'ID_from_Google_share', 'GDrive', False))
+     data_instructions.append(('.', 'Hello.txt', 'https://githubfolder/etc', 'Github', True))
+     ffdt.retrieve_data(data_instructions)
+  '''
   for data_instruction in data_instructions:
     base_path, file, base_url, method, remove = data_instruction
 
@@ -39,14 +57,25 @@ def retrieve_data(data_instructions):
         print(f"Extracting files in {file} ...")
         with ZipFile(file_path, 'r') as zippy:
           zippy.extractall(base_path)
-		  
-def clean_dir(dir):
-  for ent in os.listdir(dir):
-    ent = os.path.join(dir, ent)
+
+
+def clean_directory(directory, skip_logs=True):
+  '''
+  Cleans all files and all directories in a directory
+  Arguments: 
+    - directory (string) - the path of the directory to clean
+    - skip_logs (boolean) - default: True
+  '''
+  if not os.path.isdir(directory):
+    print(f'folder: {directory} does not exist. Skipping!')
+    return
+  
+  for ent in os.listdir(directory):
+    ent = os.path.join(directory, ent)
     if os.path.isdir(ent):
       shutil.rmtree(ent)
-    else:
+    elif not (ent.endswith('.log') and skip_logs):
       os.remove(ent) 
   
-  print(f'Cleaned directory {dir}')
+  print(f'Cleaned directory: {directory}')
 
